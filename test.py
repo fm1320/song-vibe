d7afc720bbccad939dd7f6d37da3365f09f923c4
AntMorais <antoniomorais.nn@gmail.com>
2024-03-04 18:25:30 +0000

    updaet

---
+++ test.py
@@ -0,0 +1,11 @@
+from song_vibe.inference import suggest
+
+
+def test():
+    song_desc = "chilly afternoon walk in the park"
+    print(song_desc)
+    suggest(song_desc)
+
+
+if __name__ == "__main__":
+    test()
